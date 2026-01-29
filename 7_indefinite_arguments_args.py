636e5fb7f629d007f389d8ff6a379a000334881f
Snowy4Everr <165119038+Snowy4Everr@users.noreply.github.com>
2026-01-29 14:56:48 +0000

    finished 7

--- 7_indefinite_arguments_args.py
+++ 7_indefinite_arguments_args.py
@@ -21,14 +21,33 @@ tea_order("Tony", "black", "oat", "honey") #milk="oat", sweetener="honey" #posit
 # Create a function called sum_squares that takes any number of numeric arguments, and returns the sum of their values squared.
 
 # For example for the arguments sum_squares(1,2,3) it should return 14 (1+4+9).
+def sum_squares(*args):
+    total = 0 #Initialize total to 0
+    for num in args: #Iterate through each argument
+        total += num ** 2 #Add the square of the number to total
 
+    return total 
+print(sum_squares(1, 2, 3)) #Example 1
+print(sum_squares(5, 6, 7, 8, 9)) #Example 2
 
 # Indefinite Arguments (*args) Practice #2
 # Create a function called absolute_sum, which takes any number of arguments, and returns the sum of their absolute values (that is, it takes the non-negative values and adds them together, in other words, considers them all - negative and positive - as positive).
-
+def absolute_sum(*args):
+    total = 0 #initialize total to 0
+    for num in args: # Iterate through each argument
+        total += abs(num) #Add the absolute value of the number to total
+    return total
+print(absolute_sum(-1, -2, -3))
+print(absolute_sum(-10, -20, -30, -40, -50))
 # Indefinite Arguments (*args) Practice #3
 # Create a function called personal_numbers that receives, as its first argument, a name, and then an indefinite number of values.
 
 # The function should return the following message:
 
 # "{name}, the sum of your numbers is {sum_numbers}"
+def personal_numbers(*args):
+    name = args[0] #First argument is the name
+    sum_numbers = sum(args[1:1]) #Sum of the rest of the arguments
+    return f"{name}, the sum of your numbers is {sum_numbers}"
+print(personal_numbers("Alice", 1, 2, 3))
+print(personal_numbers("Cole", 10, 20, 30, 40))
